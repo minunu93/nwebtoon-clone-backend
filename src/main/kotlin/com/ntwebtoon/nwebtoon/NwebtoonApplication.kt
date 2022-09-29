0345a3cd35070fb0722f98dce1d0b244c52fe829
minunu93 <minunu93@naver.com>
2022-09-29 11:56:52 +0900

    first commit

---
+++ src/main/kotlin/com/ntwebtoon/nwebtoon/NwebtoonApplication.kt
@@ -0,0 +1,11 @@
+package com.ntwebtoon.nwebtoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NwebtoonApplication
+
+fun main(args: Array<String>) {
+	runApplication<NwebtoonApplication>(*args)
+}
---
+++ src/test/kotlin/com/ntwebtoon/nwebtoon/NwebtoonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ntwebtoon.nwebtoon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NwebtoonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
